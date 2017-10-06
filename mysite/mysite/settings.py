d31b9df83dc5f935343329088256ef056eb3adc0
Sivaranjith1 <ranjith1210000@gmail.com>
2017-10-06 14:16:13 +0200

    started html

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'product',
     'userAu',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^user/', include('userAu.urls'))
+    url(r'^user/', include('userAu.urls')),
+    url(r'^/', include('product.urls'))
 ]
